44cf4afb31e3de21a1930930fb28cc77511122ff
Brandon271004 <utm22030600@utma.edu.mx>
2024-06-10 13:29:57 -0600

    main

--- src/videogames_frontend/src/App.jsx
+++ src/videogames_frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 import { videogames_backend } from 'declarations/videogames_backend';
-import { Container, Row, Col, Card, Table, Button, Modal } from 'react-bootstrap'
-import Swal from 'sweetalert2'
+import { Container, Row, Col, Card, Table, Button } from 'react-bootstrap'
+import  Swal from 'sweetalert2'
 import { useNavigate } from 'react-router-dom';
-import FormVideogame from './Create';
-
 function App() {
   const [videogames, setGames] = useState([]);
-  const [show, setShow] = useState((false));
   const navigate = useNavigate();
   useEffect(() => {
     getGames();
@@ -32,8 +29,8 @@ function App() {
                 <Card.Title> Listado de Videojuegos</Card.Title>
               </Col>
               <Col className='m-3'>
-                <Button variant="success" onClick={() => navigate('/agregar-videojuego')}>Crear videojuego</Button>
-              </Col>
+                <Button variant="success" onClick={()=>navigate('/agregar-videojuego')}>Crear videojuego</Button>
+                </Col>
             </Row>
             <Table striped bordered hover variant="dark">
               <thead>
@@ -42,7 +39,6 @@ function App() {
                   <th>Titulo</th>
                   <th>Descripcion</th>
                   <th>Rating</th>
-                  <th>Opciones</th>
                 </tr>
               </thead>
               <tbody>
@@ -55,16 +51,6 @@ function App() {
                         <td>{games.title}</td>
                         <td>{games.description}</td>
                         <td>{Number(games.rating)}</td>
-                        <td>
-                          <Row>
-                            <Col>
-                              <Button variant="info" >Editar </Button>
-                            </Col>
-                            <Col>
-                              <Button variant='danger'> Eliminar </Button>
-                            </Col>
-                          </Row>
-                        </td>
                       </tr>
                     ))
                     : <tr></tr>
@@ -74,20 +60,6 @@ function App() {
           </Card.Body>
         </Card>
       </Row>
-      <Modal show={show}>
-        <Modal.Header closeButton>
-          <Modal.Title> Editar Videojuego! </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <FormVideogame
-            id={Number(games.id)}
-            pTitle={games.title}
-            pDescription={games.description}
-            pRating={games.rating}
-            isEditable={true}
-          />
-        </Modal.Body>
-      </Modal>
     </Container>
   );
 }
